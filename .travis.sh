b89975cb819e26c8d0af26dccc815dad7712e919
Adam Becker <beannaich@gmail.com>
2017-05-03 18:19:56 -0600

    Minor formatting fixes, and adding badges to README.md

--- .travis.sh
+++ .travis.sh
@@ -1,36 +1,34 @@
 #/bin/bash
 
-travis_before_install() 
-{
-	cd ..
-	if [ "$TARGET_OS" = "Linux" ]; then
-		sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test;
-		sudo apt-get update -qq;
-		sudo apt-get install -qq ;
-		sudo apt-get install -qq libsdl2-dev gcc-5 g++-5 cmake;
-	elif [ "$TARGET_OS" = "OSX" ]; then
-		brew update
-		brew install sdl2
-	fi;
 
+travis_before_install() {
+  cd ..
+  if [ "$TARGET_OS" = "Linux" ]; then
+    sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test;
+    sudo apt-get update -qq;
+    sudo apt-get install -qq ;
+    sudo apt-get install -qq libsdl2-dev gcc-5 g++-5 cmake;
+  elif [ "$TARGET_OS" = "OSX" ]; then
+    brew update
+    brew install sdl2
+  fi;
 }
 
-travis_script()
-{
 
-	mkdir build
-	pushd build
-	
-	if [ "$TARGET_OS" = "Linux" ]; then
-		if [ "$CXX" = "g++" ]; then export CXX="g++-5" CC="gcc-5"; fi
-		cmake .. -G"$BUILD_TYPE" -DCMAKE_BUILD_TYPE=Release;
-		cmake --build .
-	elif [ "$TARGET_OS" = "OSX" ]; then
-		cmake .. -G"$BUILD_TYPE"
-		cmake --build . --config Release
-	fi;
+travis_script() {
+  mkdir build
+  pushd build
 
-	popd
+  if [ "$TARGET_OS" = "Linux" ]; then
+    if [ "$CXX" = "g++" ]; then export CXX="g++-5" CC="gcc-5"; fi
+    cmake .. -G"$BUILD_TYPE" -DCMAKE_BUILD_TYPE=Release;
+    cmake --build .
+  elif [ "$TARGET_OS" = "OSX" ]; then
+    cmake .. -G"$BUILD_TYPE"
+    cmake --build . --config Release
+  fi;
+
+  popd
 }
 
 
