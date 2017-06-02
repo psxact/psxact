3b8d549b738d2d66e3f5636f4b0afe89fed16892
Adam <beannaich@gmail.com>
2017-06-01 20:43:38 -0600

    Changing a few function names to fix ambiguity errors in CLion.

--- src/cpu/cpu_code.cpp
+++ src/cpu/cpu_code.cpp
@@ -44,11 +44,11 @@ static inline void set_rt(cpu_state_t *state, uint32_t value) {
 }
 
 template<bool forwarded = false>
-static inline uint32_t rt(cpu_state_t *state) {
+static inline uint32_t get_rt(cpu_state_t *state) {
   return get_register<forwarded>(state, cpu::decoder::rt(state));
 }
 
-static inline uint32_t rs(cpu_state_t *state) {
+static inline uint32_t get_rs(cpu_state_t *state) {
   return get_register(state, cpu::decoder::rs(state));
 }
 
@@ -57,8 +57,8 @@ static inline uint32_t rs(cpu_state_t *state) {
 // --============--
 
 void cpu::op_add(cpu_state_t *state) {
-  auto x = rs(state);
-  auto y = rt(state);
+  auto x = get_rs(state);
+  auto y = get_rt(state);
   auto z = x + y;
 
   if (overflow(x, y, z)) {
@@ -70,7 +70,7 @@ void cpu::op_add(cpu_state_t *state) {
 }
 
 void cpu::op_addi(cpu_state_t *state) {
-  auto x = rs(state);
+  auto x = get_rs(state);
   auto y = decoder::iconst(state);
   auto z = x + y;
 
@@ -83,44 +83,44 @@ void cpu::op_addi(cpu_state_t *state) {
 }
 
 void cpu::op_addiu(cpu_state_t *state) {
-  set_rt(state, rs(state) + decoder::iconst(state));
+  set_rt(state, get_rs(state) + decoder::iconst(state));
 }
 
 void cpu::op_addu(cpu_state_t *state) {
-  set_rd(state, rs(state) + rt(state));
+  set_rd(state, get_rs(state) + get_rt(state));
 }
 
 void cpu::op_and(cpu_state_t *state) {
-  set_rd(state, rs(state) & rt(state));
+  set_rd(state, get_rs(state) & get_rt(state));
 }
 
 void cpu::op_andi(cpu_state_t *state) {
-  set_rt(state, rs(state) & decoder::uconst(state));
+  set_rt(state, get_rs(state) & decoder::uconst(state));
 }
 
 void cpu::op_beq(cpu_state_t *state) {
-  if (rs(state) == rt(state)) {
+  if (get_rs(state) == get_rt(state)) {
     state->regs.next_pc = state->regs.pc + (decoder::iconst(state) << 2);
     state->is_branch = true;
   }
 }
 
 void cpu::op_bgtz(cpu_state_t *state) {
-  if (int32_t(rs(state)) > 0) {
+  if (int32_t(get_rs(state)) > 0) {
     state->regs.next_pc = state->regs.pc + (decoder::iconst(state) << 2);
     state->is_branch = true;
   }
 }
 
 void cpu::op_blez(cpu_state_t *state) {
-  if (int32_t(rs(state)) <= 0) {
+  if (int32_t(get_rs(state)) <= 0) {
     state->regs.next_pc = state->regs.pc + (decoder::iconst(state) << 2);
     state->is_branch = true;
   }
 }
 
 void cpu::op_bne(cpu_state_t *state) {
-  if (rs(state) != rt(state)) {
+  if (get_rs(state) != get_rt(state)) {
     state->regs.next_pc = state->regs.pc + (decoder::iconst(state) << 2);
     state->is_branch = true;
   }
@@ -136,8 +136,8 @@ void cpu::op_bxx(cpu_state_t *state) {
   // bltz rs,$nnnn
   // bltzal rs,$nnnn
   bool condition = (state->code & (1 << 16))
-    ? int32_t(rs(state)) >= 0
-    : int32_t(rs(state)) <  0;
+    ? int32_t(get_rs(state)) >= 0
+    : int32_t(get_rs(state)) <  0;
 
   if ((state->code & 0x1e0000) == 0x100000) {
     state->regs.gp[31] = state->regs.next_pc;
@@ -154,7 +154,7 @@ void cpu::op_cop0(cpu_state_t *state) {
   default: op_und(state); return;
 
   case 0x00: set_rt(state, state->cop0.regs[decoder::rd(state)]); return; // mfc0 rt,rd
-  case 0x04: state->cop0.regs[decoder::rd(state)] = rt(state); return; // mtc0 rt,rd
+  case 0x04: state->cop0.regs[decoder::rd(state)] = get_rt(state); return; // mtc0 rt,rd
 
   case 0x10:
     switch (state->code & 63) {
@@ -178,8 +178,8 @@ void cpu::op_cop3(cpu_state_t *state) {
 }
 
 void cpu::op_div(cpu_state_t *state) {
-  auto dividend = int32_t(rs(state));
-  auto divisor = int32_t(rt(state));
+  auto dividend = int32_t(get_rs(state));
+  auto divisor = int32_t(get_rt(state));
 
   if (dividend == 0x80000000 && divisor == 0xffffffff) {
     state->regs.lo = 0x80000000;
@@ -200,8 +200,8 @@ void cpu::op_div(cpu_state_t *state) {
 }
 
 void cpu::op_divu(cpu_state_t *state) {
-  auto dividend = rs(state);
-  auto divisor = rt(state);
+  auto dividend = get_rs(state);
+  auto divisor = get_rt(state);
 
   if (divisor) {
     state->regs.lo = dividend / divisor;
@@ -227,19 +227,19 @@ void cpu::op_jal(cpu_state_t *state) {
 void cpu::op_jalr(cpu_state_t *state) {
   auto ra = state->regs.next_pc;
 
-  state->regs.next_pc = rs(state);
+  state->regs.next_pc = get_rs(state);
   set_rd(state, ra);
 
   state->is_branch = true;
 }
 
 void cpu::op_jr(cpu_state_t *state) {
-  state->regs.next_pc = rs(state);
+  state->regs.next_pc = get_rs(state);
   state->is_branch = true;
 }
 
 void cpu::op_lb(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   auto data = read_data(state, bus::BUS_WIDTH_BYTE, address);
   data = utility::sclip<8>(data);
 
@@ -247,14 +247,14 @@ void cpu::op_lb(cpu_state_t *state) {
 }
 
 void cpu::op_lbu(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   auto data = read_data(state, bus::BUS_WIDTH_BYTE, address);
 
   set_rt<1>(state, data);
 }
 
 void cpu::op_lh(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   if (address & 1) {
     enter_exception(state, 0x4);
   }
@@ -267,7 +267,7 @@ void cpu::op_lh(cpu_state_t *state) {
 }
 
 void cpu::op_lhu(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   if (address & 1) {
     enter_exception(state, 0x4);
   }
@@ -283,7 +283,7 @@ void cpu::op_lui(cpu_state_t *state) {
 }
 
 void cpu::op_lw(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   if (address & 3) {
     enter_exception(state, 0x4);
   }
@@ -311,28 +311,28 @@ void cpu::op_lwc3(cpu_state_t *state) {
 }
 
 void cpu::op_lwl(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   auto data = read_data(state, bus::BUS_WIDTH_WORD, address & ~3);
 
   switch (address & 3) {
-  default: data = (data << 24) | (rt<1>(state) & 0x00ffffff); break;
-  case  1: data = (data << 16) | (rt<1>(state) & 0x0000ffff); break;
-  case  2: data = (data <<  8) | (rt<1>(state) & 0x000000ff); break;
-  case  3: data = (data <<  0) | (rt<1>(state) & 0x00000000); break;
+  default: data = (data << 24) | (get_rt<1>(state) & 0x00ffffff); break;
+  case  1: data = (data << 16) | (get_rt<1>(state) & 0x0000ffff); break;
+  case  2: data = (data <<  8) | (get_rt<1>(state) & 0x000000ff); break;
+  case  3: data = (data <<  0) | (get_rt<1>(state) & 0x00000000); break;
   }
 
   set_rt<1>(state, data);
 }
 
 void cpu::op_lwr(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   auto data = read_data(state, bus::BUS_WIDTH_WORD, address & ~3);
 
   switch (address & 3) {
-  default: data = (data >>  0) | (rt<1>(state) & 0x00000000); break;
-  case  1: data = (data >>  8) | (rt<1>(state) & 0xff000000); break;
-  case  2: data = (data >> 16) | (rt<1>(state) & 0xffff0000); break;
-  case  3: data = (data >> 24) | (rt<1>(state) & 0xffffff00); break;
+  default: data = (data >>  0) | (get_rt<1>(state) & 0x00000000); break;
+  case  1: data = (data >>  8) | (get_rt<1>(state) & 0xff000000); break;
+  case  2: data = (data >> 16) | (get_rt<1>(state) & 0xffff0000); break;
+  case  3: data = (data >> 24) | (get_rt<1>(state) & 0xffffff00); break;
   }
 
   set_rt<1>(state, data);
@@ -347,16 +347,16 @@ void cpu::op_mflo(cpu_state_t *state) {
 }
 
 void cpu::op_mthi(cpu_state_t *state) {
-  state->regs.hi = rs(state);
+  state->regs.hi = get_rs(state);
 }
 
 void cpu::op_mtlo(cpu_state_t *state) {
-  state->regs.lo = rs(state);
+  state->regs.lo = get_rs(state);
 }
 
 void cpu::op_mult(cpu_state_t *state) {
-  auto s = int32_t(rs(state));
-  auto t = int32_t(rt(state));
+  auto s = int32_t(get_rs(state));
+  auto t = int32_t(get_rt(state));
 
   int64_t result = int64_t(s) * int64_t(t);
   state->regs.lo = uint32_t(result >> 0);
@@ -364,8 +364,8 @@ void cpu::op_mult(cpu_state_t *state) {
 }
 
 void cpu::op_multu(cpu_state_t *state) {
-  auto s = rs(state);
-  auto t = rt(state);
+  auto s = get_rs(state);
+  auto t = get_rt(state);
 
   uint64_t result = uint64_t(s) * uint64_t(t);
   state->regs.lo = uint32_t(result >> 0);
@@ -373,79 +373,79 @@ void cpu::op_multu(cpu_state_t *state) {
 }
 
 void cpu::op_nor(cpu_state_t *state) {
-  set_rd(state, ~(rs(state) | rt(state)));
+  set_rd(state, ~(get_rs(state) | get_rt(state)));
 }
 
 void cpu::op_or(cpu_state_t *state) {
-  set_rd(state, rs(state) | rt(state));
+  set_rd(state, get_rs(state) | get_rt(state));
 }
 
 void cpu::op_ori(cpu_state_t *state) {
-  set_rt(state, rs(state) | decoder::uconst(state));
+  set_rt(state, get_rs(state) | decoder::uconst(state));
 }
 
 void cpu::op_sb(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
-  auto data = rt(state);
+  auto address = get_rs(state) + decoder::iconst(state);
+  auto data = get_rt(state);
 
   write_data(state, bus::BUS_WIDTH_BYTE, address, data);
 }
 
 void cpu::op_sh(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   if (address & 1) {
     enter_exception(state, 0x5);
   }
   else {
-    auto data = rt(state);
+    auto data = get_rt(state);
 
     write_data(state, bus::BUS_WIDTH_HALF, address, data);
   }
 }
 
 void cpu::op_sll(cpu_state_t *state) {
-  set_rd(state, rt(state) << decoder::sa(state));
+  set_rd(state, get_rt(state) << decoder::sa(state));
 }
 
 void cpu::op_sllv(cpu_state_t *state) {
-  set_rd(state, rt(state) << rs(state));
+  set_rd(state, get_rt(state) << get_rs(state));
 }
 
 void cpu::op_slt(cpu_state_t *state) {
-  set_rd(state, int32_t(rs(state)) < int32_t(rt(state)) ? 1 : 0);
+  set_rd(state, int32_t(get_rs(state)) < int32_t(get_rt(state)) ? 1 : 0);
 }
 
 void cpu::op_slti(cpu_state_t *state) {
-  set_rt(state, int32_t(rs(state)) < int32_t(decoder::iconst(state)) ? 1 : 0);
+  set_rt(state, int32_t(get_rs(state)) < int32_t(decoder::iconst(state)) ? 1 : 0);
 }
 
 void cpu::op_sltiu(cpu_state_t *state) {
-  set_rt(state, rs(state) < decoder::iconst(state) ? 1 : 0);
+  set_rt(state, get_rs(state) < decoder::iconst(state) ? 1 : 0);
 }
 
 void cpu::op_sltu(cpu_state_t *state) {
-  set_rd(state, rs(state) < rt(state) ? 1 : 0);
+  set_rd(state, get_rs(state) < get_rt(state) ? 1 : 0);
 }
 
 void cpu::op_sra(cpu_state_t *state) {
-  set_rd(state, int32_t(rt(state)) >> decoder::sa(state));
+  set_rd(state, int32_t(get_rt(state)) >> decoder::sa(state));
 }
 
 void cpu::op_srav(cpu_state_t *state) {
-  set_rd(state, int32_t(rt(state)) >> rs(state));
+  set_rd(state, int32_t(get_rt(state)) >> get_rs(state));
 }
 
 void cpu::op_srl(cpu_state_t *state) {
-  set_rd(state, rt(state) >> decoder::sa(state));
+  set_rd(state, get_rt(state) >> decoder::sa(state));
 }
 
 void cpu::op_srlv(cpu_state_t *state) {
-  set_rd(state, rt(state) >> rs(state));
+  set_rd(state, get_rt(state) >> get_rs(state));
 }
 
 void cpu::op_sub(cpu_state_t *state) {
-  auto x = rs(state);
-  auto y = rt(state);
+  auto x = get_rs(state);
+  auto y = get_rt(state);
   auto z = x - y;
 
   if (overflow(x, ~y, z)) {
@@ -457,16 +457,16 @@ void cpu::op_sub(cpu_state_t *state) {
 }
 
 void cpu::op_subu(cpu_state_t *state) {
-  set_rd(state, rs(state) - rt(state));
+  set_rd(state, get_rs(state) - get_rt(state));
 }
 
 void cpu::op_sw(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   if (address & 3) {
     enter_exception(state, 0x5);
   }
   else {
-    auto data = rt(state);
+    auto data = get_rt(state);
 
     write_data(state, bus::BUS_WIDTH_WORD, address, data);
   }
@@ -489,28 +489,28 @@ void cpu::op_swc3(cpu_state_t *state) {
 }
 
 void cpu::op_swl(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   auto data = read_data(state, bus::BUS_WIDTH_WORD, address & ~3);
 
   switch (address & 3) {
-  default: data = (data & 0xffffff00) | (rt(state) >> 24); break;
-  case  1: data = (data & 0xffff0000) | (rt(state) >> 16); break;
-  case  2: data = (data & 0xff000000) | (rt(state) >>  8); break;
-  case  3: data = (data & 0x00000000) | (rt(state) >>  0); break;
+  default: data = (data & 0xffffff00) | (get_rt(state) >> 24); break;
+  case  1: data = (data & 0xffff0000) | (get_rt(state) >> 16); break;
+  case  2: data = (data & 0xff000000) | (get_rt(state) >>  8); break;
+  case  3: data = (data & 0x00000000) | (get_rt(state) >>  0); break;
   }
 
   write_data(state, bus::BUS_WIDTH_WORD, address & ~3, data);
 }
 
 void cpu::op_swr(cpu_state_t *state) {
-  auto address = rs(state) + decoder::iconst(state);
+  auto address = get_rs(state) + decoder::iconst(state);
   auto data = read_data(state, bus::BUS_WIDTH_WORD, address & ~3);
 
   switch (address & 3) {
-  default: data = (data & 0x00000000) | (rt(state) <<  0); break;
-  case  1: data = (data & 0x000000ff) | (rt(state) <<  8); break;
-  case  2: data = (data & 0x0000ffff) | (rt(state) << 16); break;
-  case  3: data = (data & 0x00ffffff) | (rt(state) << 24); break;
+  default: data = (data & 0x00000000) | (get_rt(state) <<  0); break;
+  case  1: data = (data & 0x000000ff) | (get_rt(state) <<  8); break;
+  case  2: data = (data & 0x0000ffff) | (get_rt(state) << 16); break;
+  case  3: data = (data & 0x00ffffff) | (get_rt(state) << 24); break;
   }
 
   write_data(state, bus::BUS_WIDTH_WORD, address & ~3, data);
@@ -521,11 +521,11 @@ void cpu::op_syscall(cpu_state_t *state) {
 }
 
 void cpu::op_xor(cpu_state_t *state) {
-  set_rd(state, rs(state) ^ rt(state));
+  set_rd(state, get_rs(state) ^ get_rt(state));
 }
 
 void cpu::op_xori(cpu_state_t *state) {
-  set_rt(state, rs(state) ^ decoder::uconst(state));
+  set_rt(state, get_rs(state) ^ decoder::uconst(state));
 }
 
 void cpu::op_und(cpu_state_t *state) {
