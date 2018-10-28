5d23ccfb163a1edebe36746897a3c6f272263fc2
Adam Becker <im.adam.becker@gmail.com>
2018-10-27 20:42:16 -0600

    DRY up the string decoders

--- src/cpu/bios/decoder.cpp
+++ src/cpu/bios/decoder.cpp
@@ -19,12 +19,12 @@ void decoder_t::decode_a(uint32_t pc, uint32_t function, uint32_t *args) {
     case 0x00: {
       std::string file_name = decode_string(args[0]);
       fprintf(log, "FileOpen(\"%s\", %d)\n", file_name.c_str(), args[1]);
-      return;
+      break;
     }
 
     case 0x13:
       fprintf(log, "SaveState(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x17: {
       std::string a = decode_string(args[0]);
@@ -33,7 +33,7 @@ void decoder_t::decode_a(uint32_t pc, uint32_t function, uint32_t *args) {
         a.c_str(),
         b.c_str()
       );
-      return;
+      break;
     }
 
     case 0x18: {
@@ -44,119 +44,121 @@ void decoder_t::decode_a(uint32_t pc, uint32_t function, uint32_t *args) {
         b.c_str(),
         args[2]
       );
-      return;
+      break;
     }
 
     case 0x1b: {
       std::string src = decode_string(args[0]);
       fprintf(log, "strlen(\"%s\")\n", src.c_str());
-      return;
+      break;
     }
 
     case 0x25:
       fprintf(log, "toupper('%c')\n", args[0]);
-      return;
+      break;
 
     case 0x28:
       fprintf(log, "bzero(0x%08x, %d)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x2a:
       fprintf(log, "memcpy(0x%08x, 0x%08x, %d)\n", args[0], args[1], args[2]);
-      return;
+      break;
 
     case 0x33:
       fprintf(log, "malloc(%d)\n", args[0]);
-      return;
+      break;
 
     case 0x34:
       fprintf(log, "free(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x39:
       fprintf(log, "InitHeap(0x%08x, %d)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x3c:
       fprintf(log, "putc(\'%c\')\n", args[0]);
-      return;
+      break;
 
     case 0x3e: {
       std::string text = decode_string(args[0]);
       fprintf(log, "puts(\"%s\")\n", text.c_str());
-      return;
+      break;
     }
 
     case 0x3f: {
       std::string fmt = decode_string(args[0]);
       fprintf(log, "printf(\"%s\")\n", fmt.c_str());
-      return;
+      break;
     }
 
     case 0x44:
       fprintf(log, "FlushCache()\n");
-      return;
+      break;
 
     case 0x49:
       fprintf(log, "GPU_cw(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x70:
       fprintf(log, "_bu_init()\n");
-      return;
+      break;
 
     case 0x72:
       fprintf(log, "CdRemove()\n");
-      return;
+      break;
 
     case 0x78: {
       std::string timecode = decode_timecode(args[0]);
       fprintf(log, "CdAsyncSeekL(\"%s\")\n", timecode.c_str());
-      return;
+      break;
     }
 
     case 0x7c:
       fprintf(log, "CdAsyncGetStatus(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x7e:
       fprintf(log, "CdAsyncReadSector(%d, 0x%08x, 0x%x)\n", args[0], args[1], args[2]);
-      return;
+      break;
 
     case 0x95:
       fprintf(log, "CdInitSubFunc()\n");
-      return;
+      break;
 
     case 0x96:
       fprintf(log, "AddCDROMDevice()\n");
-      return;
+      break;
 
     case 0x97:
       fprintf(log, "AddMemCardDevice()\n");
-      return;
+      break;
 
     case 0x99:
       fprintf(log, "AddDummyTtyDevice()\n");
-      return;
+      break;
 
     case 0xa1:
       fprintf(log, "SystemErrorBootOrDiskFailure('%c', %d)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0xa2:
       fprintf(log, "EnqueueCdIntr()\n");
-      return;
+      break;
 
     case 0xa3:
       fprintf(log, "DequeueCdIntr()\n");
-      return;
+      break;
 
     case 0xa9:
       fprintf(log, "bu_callback_err_busy()\n");
-      return;
-  }
+      break;
 
-  printf("bios::a(0x%02x)\n", function);
+    default:
+      printf("bios::a(0x%02x)\n", function);
+      break;
+  }
 }
 
 
@@ -166,11 +168,11 @@ void decoder_t::decode_b(uint32_t pc, uint32_t function, uint32_t *args) {
   switch (function) {
     case 0x00:
       fprintf(log, "alloc_kernel_memory(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x07:
       fprintf(log, "DeliverEvent(0x%08x, 0x%08x)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x08:
       fprintf(log, "OpenEvent(0x%08x, 0x%08x, 0x%08x, 0x%08x)\n",
@@ -178,49 +180,49 @@ void decoder_t::decode_b(uint32_t pc, uint32_t function, uint32_t *args) {
         args[1],
         args[2],
         args[3]);
-      return;
+      break;
 
     case 0x09:
       fprintf(log, "CloseEvent(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x0b:
       fprintf(log, "TestEvent(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x0c:
       fprintf(log, "EnableEvent(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x13:
       fprintf(log, "StartPad()\n");
-      return;
+      break;
 
     case 0x17:
       fprintf(log, "ReturnFromException()\n");
-      return;
+      break;
 
     case 0x18:
       fprintf(log, "SetDefaultExitFromException()\n");
-      return;
+      break;
 
     case 0x19:
       fprintf(log, "SetCustomExitFromException(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x20:
       fprintf(log, "UnDeliverEvent(0x%08x, 0x%08x)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x32: {
       std::string file_name = decode_string(args[0]);
       fprintf(log, "FileOpen(\"%s\", %d)\n", file_name.c_str(), args[1]);
-      return;
+      break;
     }
 
     case 0x34:
       fprintf(log, "FileRead(%d, 0x%08x, %d)\n", args[0], args[1], args[2]);
-      return;
+      break;
 
     case 0x35:
       if (args[0] == 1) {
@@ -230,60 +232,62 @@ void decoder_t::decode_b(uint32_t pc, uint32_t function, uint32_t *args) {
       else {
         fprintf(log, "FileWrite(%d, 0x%08x, %d)\n", args[0], args[1], args[2]);
       }
-      return;
+      break;
 
     case 0x36:
       fprintf(log, "FileClose(%d)\n", args[0]);
-      return;
+      break;
 
     case 0x3d:
       fprintf(log, "putc(\'%c\')\n", args[0]);
-      return;
+      break;
 
     case 0x3f: {
       std::string text = decode_string(args[0]);
       fprintf(log, "puts(\"%s\")\n", text.c_str());
-      return;
+      break;
     }
 
     case 0x47:
       fprintf(log, "AddDevice(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x4a:
       fprintf(log, "InitCard(%d)\n", args[0]);
-      return;
+      break;
 
     case 0x4b:
       fprintf(log, "StartCard()\n");
-      return;
+      break;
 
     case 0x4f:
       fprintf(log, "read_card_sector(%d, %d, 0%08x)\n", args[0], args[1], args[2]);
-      return;
+      break;
 
     case 0x50:
       fprintf(log, "allow_new_card()\n");
-      return;
+      break;
 
     case 0x56:
       fprintf(log, "GetC0Table()\n");
-      return;
+      break;
 
     case 0x57:
       fprintf(log, "GetB0Table()\n");
-      return;
+      break;
 
     case 0x58:
       fprintf(log, "get_bu_callback_port()\n");
-      return;
+      break;
 
     case 0x5b:
       fprintf(log, "ChangeClearPad(%d)\n", args[0]);
-      return;
-  }
+      break;
 
-  printf("bios::b(0x%02x)\n", function);
+    default:
+      printf("bios::b(0x%02x)\n", function);
+      break;
+  }
 }
 
 
@@ -293,46 +297,71 @@ void decoder_t::decode_c(uint32_t pc, uint32_t function, uint32_t *args) {
   switch (function) {
     case 0x00:
       fprintf(log, "EnqueueTimerAndVblankIrqs(%d)\n", args[0]);
-      return;
+      break;
 
     case 0x01:
       fprintf(log, "EnqueueSyscallHandler(%d)\n", args[0]);
-      return;
+      break;
 
     case 0x02:
       fprintf(log, "SysEnqIntRP(%d, 0x%08x)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x03:
       fprintf(log, "SysDeqIntRP(%d, 0x%08x)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x07:
       fprintf(log, "InstallExceptionHandlers()\n");
-      return;
+      break;
 
     case 0x08:
       fprintf(log, "SysInitMemory(0x%08x, 0x%08x)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x0a:
       fprintf(log, "ChangeClearRCnt(%d, 0x%08x)\n", args[0], args[1]);
-      return;
+      break;
 
     case 0x0c:
       fprintf(log, "InitDefInt(%d)\n", args[0]);
-      return;
+      break;
 
     case 0x12:
       fprintf(log, "InstallDevices(0x%08x)\n", args[0]);
-      return;
+      break;
 
     case 0x1c:
       fprintf(log, "AdjustA0Table()\n");
-      return;
-  }
+      break;
 
-  printf("bios::c(0x%02x)\n", function);
+    default:
+      printf("bios::c(0x%02x)\n", function);
+      break;
+  }
+}
+
+
+static std::string char_to_string(char c) {
+  switch (c) {
+  case '\0':
+    return "\\0";
+
+  case '\\':
+    return "\\\\";
+
+  case '\n':
+    return "\\n";
+
+  case '\r':
+    return "\\r";
+
+  case '\t':
+    return "\\t";
+
+  default:
+    return std::string(1, c);
+  }
 }
 
 
@@ -344,24 +373,7 @@ std::string decoder_t::decode_string(uint32_t arg) {
     curr = (char) memory->read_byte(arg & 0x1fffffff);
     arg++;
 
-    if (curr == '\0') {
-      result += "\\0";
-    }
-    else if (curr == '\\') {
-      result += "\\\\";
-    }
-    else if (curr == '\n') {
-      result += "\\n";
-    }
-    else if (curr == '\r') {
-      result += "\\r";
-    }
-    else if (curr == '\t') {
-      result += "\\t";
-    }
-    else {
-      result += curr;
-    }
+    result += char_to_string(curr);
   }
   while (curr);
 
@@ -377,24 +389,7 @@ std::string decoder_t::decode_string(uint32_t arg, uint32_t size) {
     curr = (char) memory->read_byte(arg & 0x1fffffff);
     arg++;
 
-    if (curr == '\0') {
-      result += "\\0";
-    }
-    else if (curr == '\\') {
-      result += "\\\\";
-    }
-    else if (curr == '\n') {
-      result += "\\n";
-    }
-    else if (curr == '\r') {
-      result += "\\r";
-    }
-    else if (curr == '\t') {
-      result += "\\t";
-    }
-    else {
-      result += curr;
-    }
+    result += char_to_string(curr);
   }
 
   return result;
